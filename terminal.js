02a3e2e6a3edda14716f4db6239dc54a95908b6d
outoine15 <outoine15@gmail.com>
2025-12-04 22:54:47 +0100

    terminal command updtae

--- terminal.js
+++ terminal.js
@@ -92,24 +92,38 @@ function handleCommand(cmd) {
   if (cmd === "") return;
 
   if (cmd === "help") {
-    term.write("Available commands:\r\n - help : show this help\r\n - clear : clear the terminal");
-
-  } else if (cmd === "clear") {
+    cmdHelp();
+  } else if (cmd.match(/clear/)) {
     term.clear();
 
-  } else if (cmd === "Ni") {
-    term.write("Inclusive Computer Science :\r\n NIRD operates for more inclusive Computer Science. In an era of structural dependency towards Big Tech such as Windows, the educational system is facing more and more trials : built-in obsolecence, subscription locked products, etc... The NIRD aproach vows to fight for technological autonomy, especialy for educational teams.")
-  } else if (cmd === "R") {
-    term.write("Responsible : \r\n The NIRD aproch ");
-  } else if (cmd === ""){
+  } else if (cmd.match(/Ni/)) {
+    cmdNi();
 
-  }
-  
-  
-  
-  else {
+  } else if (cmd.match(/R/)) {
+    cmdR();
+
+  } else if (cmd.match(/D/)){
+    cmdD();
+  } else {
     term.write(`${cmd}: command not found`);
+
   }
 
   term.write("\r\n");
+}
+
+
+function cmdHelp(){
+  term.write("Available commands:\r\n - help : show this help\r\n - clear : clear the terminal");
+}
+
+function cmdNi(){
+  term.write("Inclusive Computer Science :\r\n NIRD operates for more inclusive Computer Science. In an era of structural dependency towards Big Tech such as Windows, the educational system is facing more and more trials : built-in obsolecence, subscription locked products, etc... The NIRD aproach vows to fight for technological autonomy, especialy for educational teams.");
+}
+
+function cmdR(){
+  term.write("Responsible : \r\n The NIRD aproach promotes a reasoned process when it comes to technology. In a world of constant inovation, we greatlly value data privacy and co-creation. We first and furmost connect teachers with each others, via a Tchap forum, encouraging them to exchange teaching materials and raise awarness amongst managment teams. One of our other bigest mission is to spread Open Source resources. That is why we deploy computers equiped with Linux.");
+}
+function cmdD(){
+  term.write("Durability: \r\n The NIRD aproach is ")
 }
