2bc9750852505f3f1a4d728e1c6df4a760953b7a
outoine15 <outoine15@gmail.com>
2025-12-04 23:03:34 +0100

    help edit

--- terminal.js
+++ terminal.js
@@ -111,19 +111,3 @@ function handleCommand(cmd) {
 
   term.write("\r\n");
 }
-
-
-function cmdHelp(){
-  term.write("Available commands:\r\n - help : show this help\r\n - clear : clear the terminal");
-}
-
-function cmdNi(){
-  term.write("Inclusive Computer Science :\r\n NIRD operates for more inclusive Computer Science. In an era of structural dependency towards Big Tech such as Windows, the educational system is facing more and more trials : built-in obsolecence, subscription locked products, etc... The NIRD aproach vows to fight for technological autonomy, especialy for educational teams.");
-}
-
-function cmdR(){
-  term.write("Responsible : \r\n The NIRD aproach promotes a reasoned process when it comes to technology. In a world of constant inovation, we greatlly value data privacy and co-creation. We first and furmost connect teachers with each others, via a Tchap forum, encouraging them to exchange teaching materials and raise awarness amongst managment teams. One of our other bigest mission is to spread Open Source resources. That is why we deploy computers equiped with Linux.");
-}
-function cmdD(){
-  term.write("Durability: \r\n The NIRD aproach is ")
-}
