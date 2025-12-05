776c450f84ceb12c698add81c73fe8b409aa2970
VeteaSTOLL <vetea.stoll@gmail.com>
2025-12-05 02:52:35 +0100

    couleur

--- 3D.js
+++ 3D.js
@@ -3,7 +3,7 @@ import { OBJLoader } from 'three/addons/OBJLoader.js';
 
 const canvas = document.getElementById("render");
 let camera, scene, renderer, mat, timer, previous, dt;
-let distance = 300;
+let distance = 400;
 let trex;
 
 const haloShader = {
@@ -42,18 +42,27 @@ const haloShader = {
     }
   `,
   fragmentShader: `
+    uniform float off;
     uniform vec3 uColor;
     uniform vec3 uCameraPos;
 
     varying vec3 vNormal;
     varying vec3 vWorldPos;
 
+    vec3 hsv2rgb(vec3 c) {
+        // Algo de Sam Hocevar sur stack overflow
+
+        vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+        vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+        return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+    }
+
     void main() {
       vec3 viewDir = normalize(uCameraPos - vWorldPos);
 
       float dotNV = 1.-dot(vNormal, viewDir);
 
-      gl_FragColor = vec4(uColor, pow(dotNV, 2.));
+      gl_FragColor = vec4(hsv2rgb(vec3(off*0.05,1,1)), pow(dotNV, 2.));
     }
   `
 };
